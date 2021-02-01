86802d91998d8c3ea6f58322de52068492722d4b
Jérémy Mouzin <jeremy.mouzin@gmail.com>
2021-02-01 15:14:18 +0100

    Ajout d'une liste de mots à mettre en gras + améliorations diverses

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
   const attrDesc = '[data-qa-id="adview_description_container"]';
   const spanDebut = '<span style="background-color: yellow">';
   const spanFin = '</span>';
@@ -16,41 +16,50 @@
   const divDesc = document.querySelector(attrDesc);
   const MAX_TENTATIVES_VOIR_PLUS = 5;
   const TEMPS_ENTRE_TENTATIVES_EN_MS = 200;
+  const listeDeMotsAMettreEnGras = [
+    'terrain',
+    'jardin',
+  ];
 
   function clickVoirPlus() {
-      console.log("Tentative click auto sur le bouton voir plus...");
-      let boutonVoirPlus = divDesc.querySelector('button');
-      if (boutonVoirPlus === null) {
-          if (clickVoirPlus.compteur < MAX_TENTATIVES_VOIR_PLUS) {
-              console.log(`ECHEC du click, on recommence dans ${TEMPS_ENTRE_TENTATIVES_EN_MS}ms (tentative ${clickVoirPlus.compteur})`);
-              window.setTimeout(clickVoirPlus, TEMPS_ENTRE_TENTATIVES_EN_MS);
-              clickVoirPlus.compteur++;
-          } else {
-              // On continue il n'y a pas de bouton "Voir Plus"
-              console.log("Bouton Voir Plus introuvable...");
-              mettreEnSurbrillance();
-          }
+    console.log("Tentative click auto sur le bouton voir plus...");
+    let boutonVoirPlus = divDesc.querySelector('button');
+    if (boutonVoirPlus === null) {
+      if (clickVoirPlus.compteur < MAX_TENTATIVES_VOIR_PLUS) {
+        console.log(`ECHEC du click, on recommence dans ${TEMPS_ENTRE_TENTATIVES_EN_MS}ms (tentative ${clickVoirPlus.compteur})`);
+        window.setTimeout(clickVoirPlus, TEMPS_ENTRE_TENTATIVES_EN_MS);
+        clickVoirPlus.compteur++;
       } else {
-          console.log("OK Voir plus cliqué !");
-          boutonVoirPlus.click();
-          mettreEnSurbrillance();
+        // On continue il n'y a pas de bouton "Voir Plus"
+        ameliorer();
       }
+    } else {
+      boutonVoirPlus.click();
+      ameliorer();
+    }
   }
   clickVoirPlus.compteur = 0;
 
-  function mettreEnSurbrillance() {
-      console.log("mettre en surbrillance");
-      const description = divDesc.querySelector('span');
-      if (description !== null) {
-          let correspondance;
-          while ((correspondance = terrainRegExp.exec(description.textContent)) !== null) {
-              description.innerHTML = description.innerHTML.replace(correspondance[0],
-                                                                   `${spanDebut}${correspondance[0]}${spanFin}`);
-          }
-          description.innerHTML = description.innerHTML.replace('terrain', '<strong>terrain</strong>');
-      } else {
-          console.log("Pas de correspondances");
+  function mettreEnSurbrillance(description) {
+    if (description !== null) {
+      let correspondance;
+      while ((correspondance = terrainRegExp.exec(description.textContent)) !== null) {
+        description.innerHTML = description.innerHTML.replace(correspondance[0],
+          `${spanDebut}${correspondance[0]}${spanFin}`);
       }
+    }
+  }
+
+  function mettreEnGras(description, listeDeMots) {
+    listeDeMots.forEach(mot => {
+      description.innerHTML = description.innerHTML.replace(mot, `<strong>${mot}</strong>`);
+    });
+  }
+
+  function ameliorer() {
+    const description = divDesc.querySelector('span');
+    mettreEnSurbrillance(description);
+    mettreEnGras(description, listeDeMotsAMettreEnGras);
   }
 
   // On clique sur le bouton "Voir Plus"
